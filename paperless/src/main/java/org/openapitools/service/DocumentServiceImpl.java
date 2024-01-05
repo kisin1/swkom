09354d3e728bba7afef18aac66cdf410940f110d
Maja <maja1cvetkovska@gmail.com>
2024-01-05 15:29:36 +0100

    full sprint 4 with errors

---
+++ paperless/src/main/java/org/openapitools/service/DocumentServiceImpl.java
@@ -0,0 +1,167 @@
+package org.openapitools.service;
+
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import io.minio.errors.*;
+import lombok.extern.slf4j.Slf4j;
+import org.openapitools.mapper.DocumentMapper;
+import org.openapitools.mapper.GetDocument200ResponseMapper;
+import org.openapitools.mapper.UpdateDocument200ResponseMapper;
+import org.openapitools.model.*;
+import org.openapitools.persistence.entities.DocumentEntity;
+import org.openapitools.persistence.entities.StoragePath;
+import org.openapitools.persistence.repositories.DocumentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+@Service
+@Slf4j
+public class DocumentServiceImpl implements DocumentService {
+
+    private final DocumentRepository documentRepository;
+    private final DocumentMapper documentMapper;
+    private final GetDocument200ResponseMapper getDocument200ResponseMapper;
+    private final UpdateDocument200ResponseMapper updateDocument200ResponseMapper;
+
+    private final MinioClient minioClient;
+
+    private final RabbitMQSender rabbitMQSender;
+
+
+    @Value("${minio.bucketName}")
+    private String bucketName;
+
+
+    @Autowired
+    public DocumentServiceImpl(DocumentRepository documentRepository, DocumentMapper documentMapper, GetDocument200ResponseMapper getDocument200ResponseMapper, UpdateDocument200ResponseMapper updateDocument200ResponseMapper, MinioClient minioClient, RabbitMQSender rabbitMQSender) {
+        this.documentRepository = documentRepository;
+        this.documentMapper = documentMapper;
+        this.getDocument200ResponseMapper = getDocument200ResponseMapper;
+        this.updateDocument200ResponseMapper = updateDocument200ResponseMapper;
+        this.minioClient = minioClient;
+        this.rabbitMQSender = rabbitMQSender;
+    }
+
+    @Override
+    public GetDocument200Response getDocument(Integer id, Integer page, Boolean fullPerms) {
+        DocumentEntity foundEntity =  documentRepository.getReferenceById(id);
+        return getDocument200ResponseMapper.entityToDto(foundEntity);
+    }
+
+
+    @Override
+    public void uploadDocument(DocumentDto documentDTO, MultipartFile file) {
+        // TODO: document variable is unused yet
+
+        documentDTO.setCreated(OffsetDateTime.now());
+        documentDTO.setAdded(OffsetDateTime.now());
+        documentDTO.setModified(OffsetDateTime.now());
+        documentDTO.content("");
+        documentDTO.setAdded(OffsetDateTime.now());
+
+
+        DocumentEntity documentToBeSaved = documentMapper.dtoToEntity(documentDTO);
+
+        documentToBeSaved.setChecksum("checksum");
+        documentToBeSaved.setStorageType("pdf");
+        documentToBeSaved.setMimeType("pdf");
+
+        documentRepository.save(documentToBeSaved);
+
+        String objectName = documentToBeSaved.getId() + getFileExtension(Objects.requireNonNull(file.getOriginalFilename()));
+
+        try {
+            minioClient.putObject(
+                    PutObjectArgs.builder()
+                            .bucket(bucketName)
+                            .object(objectName)
+                            .stream(file.getInputStream(), file.getSize(), -1)
+                            .contentType(file.getContentType())
+                            .build()
+            );
+        } catch (ErrorResponseException | InsufficientDataException | InternalException | InvalidKeyException |
+                 InvalidResponseException | IOException | NoSuchAlgorithmException | ServerException | XmlParserException e) {
+            log.error("Error while uploading file to Minio.");
+            throw new RuntimeException(e);
+        }
+
+        StoragePath pathToFile = new StoragePath();
+        pathToFile.setPath(bucketName + "/" + objectName);
+        pathToFile.setName(file.getOriginalFilename());
+        pathToFile.setMatch("");
+        pathToFile.setMatchingAlgorithm(0);
+        pathToFile.setIsInsensitive(false);
+
+        documentToBeSaved.setStoragePath(pathToFile);
+
+        rabbitMQSender.sendToOcrDocumentInQueue(documentToBeSaved.getStoragePath().getPath());
+
+        log.info("Document saved and added to Queue.");
+
+        documentRepository.save(documentToBeSaved);
+    }
+
+
+    @Override
+    public ResponseEntity<GetDocuments200Response> getDocuments(Integer page, Integer pageSize, String query, String ordering, List<Integer> tagsIdAll, Integer documentTypeId, Integer storagePathIdIn, Integer correspondentId, Boolean truncateContent) throws IOException {
+        List<DocumentDto> documentDTOS = new ArrayList<>();
+
+        if(query == null || query.isEmpty()) {
+            for (DocumentEntity document : documentRepository.findAll()) {
+                documentDTOS.add(documentMapper.entityToDto(document));
+            }
+        } else {
+            //search with elasticsearch
+            log.info("Elastic search");
+            }
+
+
+
+        GetDocuments200Response sampleResponse = new GetDocuments200Response();
+        sampleResponse.setCount(100);
+        sampleResponse.setNext(1);
+        sampleResponse.setPrevious(1);
+        sampleResponse.addAllItem(1);
+
+        for(DocumentDto documentDTO : documentDTOS) {
+            sampleResponse.addResultsItem(documentDTO.toGetDocuments200ResponseResultsInner());
+        }
+
+        return ResponseEntity.ok(sampleResponse);
+    }
+
+    @Override
+    public ResponseEntity<UpdateDocument200Response> updateDocument(Integer id, UpdateDocumentRequest updateDocumentRequest) {
+        DocumentEntity document = documentRepository.getReferenceById(id);
+
+        document.updateByUpdateDocumentRequest(updateDocumentRequest);
+
+        documentRepository.save(document);
+
+        UpdateDocument200Response updateDocument200Response = updateDocument200ResponseMapper.entityToDto(document);
+
+        return ResponseEntity.ok(updateDocument200Response);
+    }
+
+    private String generateRandomName() {
+        return UUID.randomUUID().toString();
+    }
+
+    private String getFileExtension(String filename) {
+        int lastDotIndex = filename.lastIndexOf(".");
+        return (lastDotIndex != -1) ? filename.substring(lastDotIndex) : "";
+    }
+
+}
